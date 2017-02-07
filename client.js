376ce0c0c3d457eec9468d25d02ac0050bb0a77c
Paul Dunkirk <pauldunkirk@gmail.com>
2017-02-07 17:03:06 -0600

    initial commit

---
+++ client.js
@@ -0,0 +1,68 @@
+// var zombieOne = 'Luke';
+//
+// var zombieTwo = 'Paul';
+// var zombieThree = 'Fred';
+//
+// var zombieArray = ['Kris', zombieTwo, 'Catherine', 'Jeremy'];
+// var zombieArrayTwo = ['Phil', 'Tom'];
+//
+// console.log(zombieArray[1]);
+// zombieArray.push(zombieOne);
+// zombieArray.push(zombieTwo);
+// zombieArray.push(zombieThree);
+// console.log(zombieArray);
+// zombieArray.unshift(zombieThree);
+// console.log(zombieArray[0]);
+// zombieArray.pop();
+// console.log(zombieArray);
+//
+// console.log(zombieArray.length);
+//
+// for(var i=0; i < zombieArray.length; i++){
+//   console.log(zombieArray[i]);
+// }
+// zombieArray[2]
+//console.log(whoIsAZombie(zombieArray));
+// //console.log(whoIsAZombie(zombieArrayTwo));
+//
+// function whoIsAZombie(zombies){
+//   // console.log('zombies', zombies);
+//   // console.log('zombies[0]: ', zombies[1]);
+//   return zombies[0] + ' and ' + zombies[1];
+// }
+// // var wayCoolNumArray = [1, 2, 3, 4, 5];
+// // var wayCoolNumArray2 = [6, 7, 8, 9, 10, 11, 12];
+// //
+// // console.log(suchAnAwesomeFunction(wayCoolNumArray));
+// // console.log(suchAnAwesomeFunction(wayCoolNumArray2));
+// //
+// // function suchAnAwesomeFunction(amazingNumericalParameterOfNumbersToBeToBeAdded) {
+// //   return amazingNumericalParameterOfNumbersToBeToBeAdded[0] + amazingNumericalParameterOfNumbersToBeToBeAdded[3];
+// // }
+
+// var ballOfString = 'yarn';
+// // var firstLetter = ballOfString.charAt(0);
+// // firstLetter = ballOfString[1];
+// var stringLength = ballOfString.length;
+// var firstTwoLetters = ballOfString.substring(0,2); //first you want, then first you don't want: inclusive and exclusive
+// var lastTwoLetter = ballOfString.substring(stringLength - 2, stringlength)
+// // want r, don't want what is past it -----  ballOfString.length-2
+// var middleTwoLetters =
+// console.log(firstLetter);
+
+//
+// var commaSepValues = "one, two, three, four";
+//
+// var newArray = commaSepValues.split(','); //split or delimiter at commas - we also did with space or with commas
+// console.log(newArray);
+//
+
+
+
+
+
+
+
+
+
+//some stuff
